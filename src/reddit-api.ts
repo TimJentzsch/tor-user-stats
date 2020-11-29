9b3e371acee346692ccbb407ad2f4330dbe7ab4f
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2020-11-29 13:38:20 +0100

    Add reddit config

--- src/reddit-api.ts
+++ src/reddit-api.ts
@@ -1,1 +1,12 @@
 import snoowrap from 'snoowrap';
+import FS from 'fs';
+
+/** The config for the reddit API. */
+type RedditConfig = {
+  /** The reddit client ID. */
+  clientId: string;
+};
+
+const redditConfig: RedditConfig = JSON.parse(
+  FS.readFileSync('config/reddit.config.json', 'utf-8'),
+);
