f35a32c064d14640481aec5be06d65c88fe33a5d
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2020-11-29 21:53:45 +0100

    Add transcription peak analysis

--- src/analizer.ts
+++ src/analizer.ts
@@ -22,24 +22,73 @@ function isTranscription(comment: Comment): boolean {
   return footerRegex.test(comment.body);
 }
 
+/**
+ * Gets the peak transcription count during the given duration.
+ * @param transcriptions The transcriptions to analyze.
+ * @param duration The duration to determine the peak in, in seconds.
+ */
+function getTranscriptionPeak(transcriptions: Comment[], duration: number): number {
+  let peak = 0;
+
+  for (let anchorIndex = 0; anchorIndex < transcriptions.length; anchorIndex += 1) {
+    // Take one transcription as anchor
+    const anchor = transcriptions[anchorIndex];
+    const anchorTime = anchor.created_utc;
+    let counter = 0;
+
+    // Count all transcriptions that are within the given timeframe
+    for (let index = anchorIndex; index < transcriptions.length; index += 1) {
+      const cur = transcriptions[index];
+      const curTime = cur.created_utc;
+
+      const timeDiff = anchorTime - curTime;
+
+      // Check if the post is within the given timeframe
+      if (timeDiff <= duration) {
+        counter += 1;
+      } else {
+        break;
+      }
+    }
+
+    // Update the maximum if necessary
+    if (counter > peak) {
+      peak = counter;
+    }
+  }
+
+  return peak;
+}
+
 /** Analizes the transcriptions of the given user. */
 export default async function analizeUser(userName: string): Promise<void> {
   let allCount = 0;
   let commentCount = 0;
   let transcriptionCount = 0;
 
-  await getAllUserComments(userName, (allComments) => {
-    logger.debug(`Fetched ${allComments.length} comments`);
-    allCount += allComments.length;
+  let transcriptions: Comment[] = [];
 
-    const comments = allComments.filter((comment) => isComment(comment));
-    commentCount += comments.length;
+  await getAllUserComments(userName, (newComments) => {
+    logger.debug(`Fetched ${newComments.length} comments`);
+    allCount += newComments.length;
 
-    const transcriptions = comments.filter((comment) => isTranscription(comment));
-    transcriptionCount += transcriptions.length;
+    const newValidComments = newComments.filter((comment) => isComment(comment));
+    commentCount += newValidComments.length;
+
+    const newTranscriptions = newValidComments.filter((comment) => isTranscription(comment));
+    transcriptionCount += newTranscriptions.length;
+
+    transcriptions = transcriptions.concat(newTranscriptions);
   });
 
   logger.info(
     `All comments fetched. All: ${allCount}, Comments: ${commentCount}, Transcriptions: ${transcriptionCount}`,
   );
+
+  const hourPeak = getTranscriptionPeak(transcriptions, 60 * 60); // 1h
+  const dayPeak = getTranscriptionPeak(transcriptions, 24 * 60 * 60); // 24h
+  const weekPeak = getTranscriptionPeak(transcriptions, 7 * 24 * 60 * 60); // 7d
+  const yearPeak = getTranscriptionPeak(transcriptions, 365 * 24 * 60 * 60); // 365d
+
+  logger.info(`Peaks: 1h: ${hourPeak} | 24h: ${dayPeak} | 7d: ${weekPeak} | 365d: ${yearPeak}`);
 }
