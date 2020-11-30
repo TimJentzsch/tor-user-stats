9f5458287e6ce51815b151877e01c22eda401f40
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2020-11-30 10:39:58 +0100

    Add transcription class

---
+++ src/transcription.ts
@@ -0,0 +1,83 @@
+import { Comment } from 'snoowrap';
+
+/**
+ * Regular expression to recognize transcriptions. Groups:
+ * - header: The header of the transcription.
+ * - type: The type of the transcription, e.g. 'Twitter Post'.
+ */
+const headerRegex = /^\s*(?<header>\*?Image Transcription:\*?\s*(?<type>Facebook Comments)\*?\s*-{3,})\s*/;
+/**
+ * Regular expression to recognize transcriptions. Groups:
+ * - content: The transcription content.
+ */
+const contentRegex = /(?<content>.*)/;
+/**
+ * Regular expression to recognize transcription footers. Groups:
+ * - footer: The footer of the transcription.
+ */
+// Note: There are two types of footers, one has an extra '&#32;'. Both have to be recognized.
+const footerRegex = /\s*(?<footer>\^\^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;\[If&#32;(&#32;)?you'd&#32;like&#32;more&#32;information&#32;on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;we&#32;do&#32;it,&#32;click&#32;here!\]\(https:\/\/www\.reddit\.com\/r\/TranscribersOfReddit\/wiki\/index\))\s*$/;
+/**
+ * Regular expression to recognize transcriptions. Groups:
+ * - header: The header of the transcription.
+ * - type: The type of the transcription, e.g. 'Twitter Post'.
+ * - content: The transcription content.
+ * - footer: The footer of the transcription.
+ */
+const transcriptionRegex = new RegExp(
+  headerRegex.source + contentRegex.source + footerRegex.source,
+  'm',
+);
+
+export default class Transcription {
+  /** The header of the transcription, formatted in reddit markdown. */
+  public headerMD: string;
+  /** The type of the transcription, e.g. 'Twitter Post'. */
+  public type: string;
+  /** The content of the transcription, formatted in reddit markdown. */
+  public contentMD: string;
+  /** The footer of the transcription, formatted in reddit markdown. */
+  public footerMD: string;
+
+  constructor(
+    /** The full body of the transcription, formatted in reddit markdown. */
+    public bodyMD: string,
+    /** The full body of the transcription, formatted in HTML. */
+    public bodyHTML: string,
+    /** The creation time of the transcription, in seconds since midnight 1970-01-01. */
+    public created: number,
+    /** The creation time of the transcription, in seconds since midnight 1970-01-01 UTC. */
+    public createdUTC: number,
+    /** The score of the transcription. */
+    public score: number,
+    /** The name of the subreddit, in the format 'r/subreddit'. */
+    public subredditNamePrefixed: string,
+  ) {
+    // Extract transcription-specific attributes
+    const match = transcriptionRegex.exec(bodyMD);
+
+    if (match === null || match.groups === undefined) {
+      throw new Error(`Failed to convert comment to transcription:\n<<<${bodyMD}>>>`);
+    }
+
+    this.headerMD = match.groups.headerMD;
+    this.type = match.groups.type;
+    this.contentMD = match.groups.contentMD;
+    this.footerMD = match.groups.footerMD;
+  }
+
+  /**
+   * Creates a new transcription from a comment.
+   * @param comment The comment to create the transcription from.
+   */
+  static fromComment(comment: Comment): Transcription {
+    return new Transcription(
+      comment.body,
+      comment.body_html,
+      comment.created,
+      comment.created_utc,
+      comment.score,
+      comment.subreddit_name_prefixed,
+    );
+  }
+}
