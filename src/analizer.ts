8c3bb669ba3746126a9c162a42fcfe98852c09a0
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2020-11-30 11:38:19 +0100

    Add function to calculate the average amount of transcriptions

--- src/analizer.ts
+++ src/analizer.ts
@@ -52,6 +52,32 @@ export function getTranscriptionPeak(transcriptions: Transcription[], duration: 
   return peak;
 }
 
+/**
+ * Gets the average number of transcriptions made in the given timeframe.
+ * @param transcriptions The transcriptions to analyze.
+ * @param duration The duration to get the average for, in seconds.
+ */
+export function getTranscriptionAvg(transcriptions: Transcription[], duration: number): number {
+  const count = transcriptions.length;
+
+  // Check if transcriptions have been made
+  if (count === 0) {
+    return 0;
+  }
+
+  const transcriptionStart = transcriptions[transcriptions.length - 1].createdUTC;
+  const transcriptionEnd = transcriptions[0].createdUTC;
+  const transcriptionDur = transcriptionEnd - transcriptionStart;
+
+  // If the timeframe is larger than the transcription frame, return the number of transcriptions
+  if (transcriptionDur <= duration) {
+    return count;
+  }
+
+  // Return the avg count of transcriptions in the given timeframe
+  return (duration / transcriptionDur) * count;
+}
+
 /** Analizes the transcriptions of the given user. */
 export default async function analizeUser(userName: string): Promise<void> {
   let allCount = 0;
--- tests/analizer.test.ts
+++ tests/analizer.test.ts
@@ -1,4 +1,4 @@
-import { getTranscriptionPeak } from '../src/analizer';
+import { getTranscriptionAvg, getTranscriptionPeak } from '../src/analizer';
 import Transcription from '../src/transcription';
 
 const defaultTranscriptionMD = `*Image Transcription:*
@@ -16,6 +16,7 @@ NOTE: if your post has text above an image, transcribe the text before describin
 ^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;like&#32;more&#32;information&#32;on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;we&#32;do&#32;it,&#32;click&#32;here!](https://www.reddit.com/r/TranscribersOfReddit/wiki/index)`;
 
 describe('Analizer', () => {
+  // Transcription peak
   describe('getTranscriptionPeak', () => {
     test('should return 0 for empty array', () => {
       const transcriptions: Transcription[] = [];
@@ -51,6 +52,32 @@ describe('Analizer', () => {
       ];
       const actual = getTranscriptionPeak(transcriptions, duration);
 
+      expect(actual).toBe(1);
+    });
+  });
+  // Transcription average
+  describe('getTranscriptionAvg', () => {
+    test('should return count for larger timeframe', () => {
+      const duration = 60 * 60; // 1h
+      const transcriptions: Transcription[] = [
+        new Transcription(defaultTranscriptionMD, '', 1923019 + 20, 12, 'r/Old_Recipes'),
+        new Transcription(defaultTranscriptionMD, '', 1923019 + 10, 12, 'r/Old_Recipes'),
+        new Transcription(defaultTranscriptionMD, '', 1923019, 12, 'r/Old_Recipes'),
+      ];
+      const actual = getTranscriptionAvg(transcriptions, duration);
+
+      expect(actual).toBe(3);
+    });
+    test('should return average for smaller timeframe', () => {
+      const duration = 60 * 60; // 1h
+      const transcriptions: Transcription[] = [
+        new Transcription(defaultTranscriptionMD, '', 1923019 + duration * 3, 12, 'r/Old_Recipes'),
+        new Transcription(defaultTranscriptionMD, '', 1923019 + duration * 2, 12, 'r/Old_Recipes'),
+        new Transcription(defaultTranscriptionMD, '', 1923019, 12, 'r/Old_Recipes'),
+      ];
+      const actual = getTranscriptionAvg(transcriptions, duration);
+
+      // 3 transcriptions in 3h => 1 transcription in 1h
       expect(actual).toBe(1);
     });
   });
