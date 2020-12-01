2b1503122b4b130d56734de44345b4a28b06f585
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-12-01 23:30:26 +0100

    Remove tag colors from the code

--- src/tags.ts
+++ src/tags.ts
@@ -4,8 +4,6 @@ export class Tag {
     public id: string,
     /** The name of the tag. */
     public name: string,
-    /** The hex color of the tag. */
-    public color: string,
   ) {}
 
   public toString(): string {
@@ -15,14 +13,14 @@ export class Tag {
 
 export class CountTag extends Tag {
   constructor(
+    /** The name of the tag. */
     name: string,
-    color: string,
     /** The lower bound for the transcription count. */
     public lowerBound: number,
     /** The upper bound for the transcription count. */
     public upperBound: number,
   ) {
-    super(name.toLocaleLowerCase(), name, color);
+    super(name.toLocaleLowerCase(), name);
   }
 
   public toString(): string {
@@ -33,10 +31,10 @@ export class CountTag extends Tag {
 // - SPECIAL TAGS -----------------------------------------
 
 /** 100 transcriptions in 24h. */
-const twentyFour = new Tag('twentyFour', '100/24h Club', '#fcec3c');
+const twentyFour = new Tag('twentyFour', '100/24h Club');
 
 /** Mod on r/TranscribersOfReddit. */
-const mod = new Tag('mod', 'Mod', '#fc3c46');
+const mod = new Tag('mod', 'Mod');
 
 export const specialTags = {
   twentyFour,
@@ -46,34 +44,34 @@ export const specialTags = {
 // - COUNT TAGS -------------------------------------------
 
 /** Visitor, 0. */
-const visitor = new CountTag('Initiate', '#aaaaaa', 0, 0);
+const visitor = new CountTag('Initiate', 0, 0);
 
 /** Initiate, 1-49. */
-const initiate = new CountTag('Initiate', '#eeeeee', 1, 49);
+const initiate = new CountTag('Initiate', 1, 49);
 
 /** Green, 50-99. */
-const green = new CountTag('Green', '#46d160', 50, 99);
+const green = new CountTag('Green', 50, 99);
 
 /** Teal, 100-249. */
-const teal = new CountTag('Teal', '#0aa18f', 100, 249);
+const teal = new CountTag('Teal', 100, 249);
 
 /** Purple, 250-499. */
-const purple = new CountTag('Purple', '#7f00ff', 250, 499);
+const purple = new CountTag('Purple', 250, 499);
 
 /** Gold, 500-999. */
-const gold = new CountTag('Gold', '#ffffff', 500, 999);
+const gold = new CountTag('Gold', 500, 999);
 
 /** Diamond, 1000-2499. */
-const diamond = new CountTag('Diamond', '#b9f2ff', 1000, 2499);
+const diamond = new CountTag('Diamond', 1000, 2499);
 
 /** Ruby, 2500-4999. */
-const ruby = new CountTag('Ruby', '#ff005d', 2500, 4999);
+const ruby = new CountTag('Ruby', 2500, 4999);
 
 /** Topaz, 5000-9999. */
-const topaz = new CountTag('Topaz', '#ff6200', 5000, 9999);
+const topaz = new CountTag('Topaz', 5000, 9999);
 
 /** Jade, 10000+. */
-const jade = new CountTag('Jade', '#307250', 10000, Infinity);
+const jade = new CountTag('Jade', 10000, Infinity);
 
 /** Tags for the transcription count. */
 export const countTags = [visitor, initiate, green, teal, purple, gold, diamond, ruby, topaz, jade];
