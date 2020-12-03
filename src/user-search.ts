0ca3f2d2355358a5dc4cbf429ef23763ec4b4f5b
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-12-03 12:18:28 +0100

    Add small user search for user display

--- src/user-search.ts
+++ src/user-search.ts
@@ -1,15 +1,14 @@
-function searchUser() {
-  const input = document.getElementById('user-input') as HTMLInputElement;
+function searchUserMain() {
+  const input = document.getElementById('main-user-input') as HTMLInputElement;
   const userName = input.value;
 
   window.location.href = `/user.html?user=${userName}`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const searchForm = document.getElementById('search-form');
+  const searchForm = document.getElementById('main-search-form');
   searchForm?.addEventListener('submit', () => {
-    console.debug('Search submit');
-    searchUser();
+    searchUserMain();
     return false;
   });
 });
