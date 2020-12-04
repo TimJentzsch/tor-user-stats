7e3479941521495fda2e63106f5a4dc363b16ad0
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-12-04 18:27:14 +0100

    Add comment interface

---
+++ src/comment.ts
@@ -0,0 +1,14 @@
+export default interface Comment {
+  /** The unique ID of the comment. */
+  id: string;
+  /** The body of the comment, formatted in Reddit markdown. */
+  body: string;
+  /** The body of the comment, formatted in HTML. */
+  body_html: string;
+  /** The comment creation time, in seconds since midnight 1970-01-01 UTC. */
+  created_utc: number;
+  /** The karma score of the comment. */
+  score: number;
+  /** The name of the subreddit, in the form 'r/subreddit'. */
+  subreddit_name_prefixed: string;
+}
