cc73c3ae18324c44aae9b8a285ee29d99909ae75
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-12-05 21:38:39 +0100

    Unify common parts of the rate algorithm

--- src/stats/rate.ts
+++ src/stats/rate.ts
@@ -1,22 +1,34 @@
 import Transcription from '../transcription';
-import { gammaPeak, karmaPeak } from './peak';
+import { gammaPeak, karmaPeak, PeakStats } from './peak';
 
-type RateData = {
+export type RateStats = {
   /** The date of the data. */
   date: Date;
   /** The rate at the date. */
   rate: number;
 };
 
-export function gammaRate(transcriptions: Transcription[], duration: number): RateData[] {
-  const data: RateData[] = [];
+/**
+ * Determines the rate of the transcriptions.
+ * @param transcriptions The transcriptions to determine the rate of.
+ * @param duration The duration of the timeframe.
+ * @param valuefn The value of each transcription.
+ * @param peakfn The peak of the transcriptions.
+ */
+export function transcriptionRateBy(
+  transcriptions: Transcription[],
+  duration: number,
+  valuefn: (transcription: Transcription) => number,
+  peakfn: (transcriptions: Transcription[], duration: number) => PeakStats,
+): RateStats[] {
+  const data: RateStats[] = [];
 
   if (transcriptions.length === 0) {
     return data;
   }
 
   // The peak should be included in the rate diagram
-  const peak = gammaPeak(transcriptions, duration);
+  const peak = peakfn(transcriptions, duration);
 
   const start = transcriptions[transcriptions.length - 1].createdUTC;
   const end = transcriptions[0].createdUTC;
@@ -34,7 +46,7 @@ export function gammaRate(transcriptions: Transcription[], duration: number): Ra
   while (curIndex >= 0 && curDate <= end) {
     // Add all transcriptions in that timeframe to the rate
     while (curIndex >= 0 && transcriptions[curIndex].createdUTC <= curDate + duration) {
-      curRate += 1;
+      curRate += valuefn(transcriptions[curIndex]);
       curIndex -= 1;
     }
     // Save the timeframe
@@ -50,44 +62,26 @@ export function gammaRate(transcriptions: Transcription[], duration: number): Ra
   return data;
 }
 
-export function karmaRate(transcriptions: Transcription[], duration: number): RateData[] {
-  const data: RateData[] = [];
+/**
+ * Determines the gamma rate of the transcriptions.
+ * @param transcriptions The transcriptions to determine the gamma rate of.
+ * @param duration The duration of the timeframe.
+ */
+export function gammaRate(transcriptions: Transcription[], duration: number): RateStats[] {
+  // Each transcription is worth 1 gamma
+  return transcriptionRateBy(transcriptions, duration, () => 1, gammaPeak);
+}
 
-  if (transcriptions.length === 0) {
-    return data;
-  }
-
-  // The peak should be included in the rate diagram
-  const peak = karmaPeak(transcriptions, duration);
-
-  const start = transcriptions[transcriptions.length - 1].createdUTC;
-  const end = transcriptions[0].createdUTC;
-  let rateStart = peak.startDate.valueOf() / 1000;
-
-  // Determine rate start
-  while (rateStart > start) {
-    rateStart -= duration;
-  }
-
-  let curDate = rateStart;
-  let curRate = 0;
-  let curIndex = transcriptions.length - 1;
-
-  while (curIndex >= 0 && curDate <= end) {
-    // Add all transcriptions in that timeframe to the rate
-    while (curIndex >= 0 && transcriptions[curIndex].createdUTC <= curDate + duration) {
-      curRate += transcriptions[curIndex].score;
-      curIndex -= 1;
-    }
-    // Save the timeframe
-    data.push({
-      rate: curRate,
-      date: new Date(curDate * 1000),
-    });
-    // Move to the next timeframe
-    curRate = 0;
-    curDate += duration;
-  }
-
-  return data;
+/**
+ * Determines the karma rate of the transcriptions.
+ * @param transcriptions The transcriptions to determine the karma rate of.
+ * @param duration The duration of the timeframe.
+ */
+export function karmaRate(transcriptions: Transcription[], duration: number): RateStats[] {
+  return transcriptionRateBy(
+    transcriptions,
+    duration,
+    (transcription) => transcription.score,
+    karmaPeak,
+  );
 }
