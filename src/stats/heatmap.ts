5965e2edc3790c619d171659c105f05db7c103f7
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-03-27 17:40:42 +0100

    Fix heatmap days being assigned incorrectly

--- src/stats/heatmap.ts
+++ src/stats/heatmap.ts
@@ -47,7 +47,11 @@ export function heatmap(transcriptions: Transcription[]): HeatMap {
   // Count entries
   transcriptions.forEach((transcription) => {
     const date = new Date(transcription.createdUTC * 1000);
-    const day = date.getUTCDay();
+    // Wrap day around so that Monday is 0 and Sunday is 7
+    let day = date.getUTCDay() - 1;
+    if (day <= 0) {
+      day += 7;
+    }
     const hour = date.getUTCHours();
 
     // Increase the corresponding heat map entry
